d64f9f31f20f3b37c593d960bbcf844463731c9f
Jonah-Camacho <jonah.camacho1@gmail.com>
2025-09-10 12:04:07 -0700

    Update PCAdder_tb.v

--- PCAdder_tb.v
+++ PCAdder_tb.v
@@ -1,35 +1,22 @@
 `timescale 1ns / 1ps
 
-////////////////////////////////////////////////////////////////////////////////
-// ECE369A - Computer Architecture
-// Laboratory 1 
-// Module - PCAdder_tb.v
-// Description - Test the 'PCAdder.v' module.
-////////////////////////////////////////////////////////////////////////////////
 
-module PCAdder_tb();
+module PCAdder_tb(
 
-    reg [31:0] PCResult;
-
-    wire [31:0] PCAddResult;
-
-    PCAdder u0(
-        .PCResult(PCResult), 
-        .PCAddResult(PCAddResult)
     );
-
-	initial begin
-	#10
-    PCResult = 32'd10;
-    #10
-    PCResult = 32'd20;
-    #10
+    
+    reg [31:0] PCResult;
+    
+    wire [31:0] PCAddResult;
+    
+    PCAdder u0(.PCResult(PCResult),.PCAddResult(PCAddResult));
+    
+    initial begin
+    #20
+    PCResult = 32'd10; #20
+    PCResult = 32'd20; #20
     PCResult = 32'd100;
+    end
     
     
-    
-	
-	end
-
 endmodule
-
