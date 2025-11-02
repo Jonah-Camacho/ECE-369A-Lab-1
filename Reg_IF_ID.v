675fefe680c93a40a20912ee0df359649f74fdc3
amelde <143756441+amelde@users.noreply.github.com>
2025-11-01 17:04:48 -0700

    Add files via upload
    Added IF/ID register file

---
+++ Reg_IF_ID.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/01/2025 04:55:22 PM
+// Design Name: 
+// Module Name: Reg_IF_ID
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Reg_IF_ID(
+
+    );
+endmodule
