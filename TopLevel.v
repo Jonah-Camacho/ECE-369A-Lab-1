7ff13189adcbbf900ab32436b9e82e2ba5b7797a
amelde <143756441+amelde@users.noreply.github.com>
2025-11-03 23:22:47 -0700

    Update TopLevel.v

--- TopLevel.v
+++ TopLevel.v
@@ -26,7 +26,6 @@ module TopLevel(
     output wire [6:0] out7,
     output wire [7:0] en_out
     );
-    
     //IF
     //Address calculation, stored in Addr
     wire [31:0] PC_curr, PC_plus4, PC_next_final;
@@ -59,6 +58,8 @@ module TopLevel(
     );
     
     assign JumpAddr_IF = {PC_ID[31:28], JumpShifted_IF[27:0] };
+    //We calculate JumpAddr here, but will be determining whether to use JumpAddr, JumpRegAddr, Branch or PC+4 in EX because it is most convenient
+    //In the future, this can be changed to be done in their respective areas (e.g. can Jump/JumpReg at ID, Branch in EX, etc. without the 4x2 mux)
     
     //IF/ID Register
     wire [31:0] Instr_ID, Addr_ID, JumpAddr_ID, PC_ID;
@@ -103,10 +104,24 @@ module TopLevel(
     wire [4:0] WriteReg_WB;
     wire [31:0] WriteData_WB;
     wire [31:0] ReadData1_ID, ReadData2_ID;
-
+    
+    wire [4:0] rs_ID = Instr_ID[25:21];
+    wire [4:0] rt_ID = Instr_ID[20:16];
+    wire [4:0] rd_ID = Instr_ID[15:11];
+    wire [5:0] funct_ID = Instr_ID[5:0];
     wire [4:0] shamt_ID = Instr_ID[10:6];
     
-  
+    RegisterFile rf(
+        .Clk(Clk),
+        .Reset(Reset),
+        .RegWrite(RegWrite_WB),
+        .ReadRegister1(rs_ID),
+        .ReadRegister2(rt_ID),
+        .WriteRegister(WriteReg_WB),
+        .WriteData(WriteData_WB),
+        .ReadData1(ReadData1_ID),
+        .ReadData2(ReadData2_ID)
+    );
     
     
     //Sign/zero extending immediate based on control signal
@@ -122,7 +137,7 @@ module TopLevel(
     wire MemToReg_EX, ALUSrc_EX, RegDst_EX;
     wire [3:0] ALUop_EX;
     wire [1:0] MemSize_EX;
-    wire [31:0] ReadData1_EX, ReadData2_EX, Imm_EX, PC_EX;
+    wire [31:0] ReadData1_EX, ReadData2_EX, Imm_EX, PC_EX, JumpAddr_EX;
     wire [4:0] rs_EX, rt_EX, rd_EX, shamt_EX;
     Reg_ID_EX ID_EX(
         .clk(Clk),
@@ -132,23 +147,26 @@ module TopLevel(
         .MemRead_in(MemRead_ID),
         .Branch_in(Branch_ID),
         .MemSize_in(MemSize_ID),
+        .JumpAddr_in(JumpAddr_ID),
         .MemToReg_in(MemToReg_ID),
         .ALUSrc_in(ALUSrc_ID),
         .RegDst_in(RegDst_ID),
         .ALUop_in(ALUop_ID),
-        .ReadData1_in(ReadData1_ID),
+        .ReadData1_in(ReadData1_ID), //same as branch target
         .ReadData2_in(ReadData2_ID),
         .ExtImm_in(Imm_ID),
         .PCp4_in(PC_ID),
         .rs_in(rs_ID),
         .rt_in(rt_ID),
         .rd_in(rd_ID),
+        .funct_in(funct_ID), 
         .shamt_in(shamt_ID),
         .RegWrite_out(RegWrite_EX),
         .MemWrite_out(MemWrite_EX),
         .MemRead_out(MemRead_EX),
         .Branch_out(Branch_EX),
         .MemSize_out(MemSize_EX),
+        .JumpAddr_out(JumpAddr_EX), 
         .MemToReg_out(MemToReg_EX),
         .ALUSrc_out(ALUSrc_EX),
         .RegDst_out(RegDst_EX),
@@ -160,8 +178,55 @@ module TopLevel(
         .rs_out(rs_EX),
         .rt_out(rt_EX),
         .rd_out(rd_EX),
+        .funct_out(funct_EX), 
         .shamt_out(shamt_EX)
     );
+    
+    //EX
+    //Assign Destination Register
+    wire DestReg_EX;
+    assign DestReg_EX = (RegDst_EX == 1) ? rd_EX : rt_EX; //go to rd if RegDst = 1, otherwise go to rt
+    
+    
+    //Calculate Branch Address
+    wire [31:0] BranchAddr_EX, ImmShifted_EX;
+    
+    Shift_left_2 sl_branch(
+        .value_in(Imm_EX),
+        .value_out(ImShifted_EX)
+    );
+    
+    assign BranchAddr_EX = PC_EX + ImmShifted_EX;
+    
+    //Assign ALU secondary value
+    wire [31:0] ALU_2nd;
+    Mux_2x1 alu_src(
+        .a(ReadData2_EX),
+        .b(Imm_EX),
+        .control(ALUSrc_EX),
+        .out(ALU_2nd)
+    );
+    
+    //Determine ALU Control Signal
+    wire [3:0] ALUCtrl;
+    ALUcontrol alu_ctrl(
+        .ALUop(ALUop_EX),
+        .funct(funct_EX),
+        .ALUctrl(ALUCtrl_EX)
+    );
+    
+    //Calculating result in ALU
+    wire [31:0] ALUres_EX;
+    wire Zero_EX;
+    ALU alu(
+        .input1(rs_EX),
+        .input2(ALU_2nd),
+        .shamt(shamt_EX),
+        .op(ALUCtrl),
+        .result(ALUres_EX),
+        .Zero(Zero_EX)
+    );
+    
 
 
 
