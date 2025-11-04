209463538e904e9b4f87ee1e7ec52983f911c576
amelde <143756441+amelde@users.noreply.github.com>
2025-11-03 21:46:14 -0700

    Update TopLevel.v

--- TopLevel.v
+++ TopLevel.v
@@ -29,19 +29,19 @@ module TopLevel(
     
     //IF
     //Address calculation, stored in Addr
-    wire [31:0] PC_curr, PC_next;
+    wire [31:0] PC_curr, PC_plus4, PC_next_final;
     
-    
-    PCAdder pcadd(
-        .PCResult(PC_curr),
-        .PCAddResult(PC_next)
-    );
     
    ProgramCounter pc(
-        .Address(PC_next),
+        .Address(PC_next_final),
         .PCResult(PC_curr),
         .Reset(Reset),
         .Clk(Clk)
+    );
+    
+    PCAdder pcadd(
+        .PCResult(PC_curr),
+        .PCAddResult(PC_plus4)
     );
     
     //Instruction from address in instruction memory, stored in InstrIF
@@ -51,30 +51,32 @@ module TopLevel(
         .Instruction(Instr_IF)
     );
     
-    //Jump address calculation, stored in JumpAddr
-    wire [31:0] JumpAddr_IF;
+    //Jump address calculation, stored in JumpAddr_IF
+    wire [31:0] JumpShifted_IF, JumpAddr_IF;
     Shift_left_2 jumpAddrCalc(
-        .value_in(PC_curr),
-        .value_out(JumpAddr_IF)
+        .value_in({6'b0, Instr_IF[25:0]}),
+        .value_out(JumpShifted_IF)
     );
     
+    assign JumpAddr_IF = {PC_ID[31:28], JumpShifted_IF[27:0] };
+    
     //IF/ID Register
-    wire [31:0] Instr_ID, Addr_ID, JumpAddr_ID;
+    wire [31:0] Instr_ID, Addr_ID, JumpAddr_ID, PC_ID;
     Reg_IF_ID IF_ID(
         .clk(Clk),
         .rst(Reset),
         .en(1), //Can changle later, enabled = 1
         .instr_in(Instr_IF),
-        .pc_in(Addr_IF),
+        .pc_in(PC_plus4),
         .jump_addr_in(JumpAddr_IF),
         .instr_out(Instr_ID),
-        .pc_out(Addr_ID),
+        .pc_out(PC_ID),
         .jump_addr_out(JumpAddr_ID)
     );
     
     //ID
     wire RegWrite_ID, ALUsrc_ID, ExtOp_ID, RegDst_ID, Branch_ID, Jump_ID, Link_ID,
-    JumpRegID, MemWrite_ID, MemRead_ID, MemToReg_ID;
+    JumpReg_ID, MemWrite_ID, MemRead_ID, MemToReg_ID;
     wire [3:0] ALUop_ID;
     wire [1:0] MemSize_ID;
     
@@ -95,41 +97,17 @@ module TopLevel(
         .MemSize(MemSize_ID)
     );
     
-    //Write Back
-
+    //Storing values from GPR[rs], GPR[rt] and GPR[rt] in ReadData1 and ReadData2 _ID
+    
     wire RegWrite_WB;
-    wire mem_wb_MemOrReg; //picks if the alu or memory data(control)
-    wire [4:0] WriteReg_WB; //reg number 
-    wire [31:0] mem_wb_alu_output;
-    wire [31:0] mem_wb_read_data;
+    wire [4:0] WriteReg_WB;
     wire [31:0] WriteData_WB;
-
-
-    wire [31:0] WriteBack_Data; //the data
-    wire [4:0] WriteReg_Wb; //write adress 
-    wire RegWrite_Wb; //write data
-
-
-  Mux_2x1 WriteBack_mux(
-
-        .a(mem_wb_alu_output),
-        .b(mem_wb_read_data),
-        .control(mem_wb_MemOrReg),
-        .out(WriteBack_Data)
-
-
-    );
-
-    assign RegWrite_WB = mem_wb_regwrite && (mem_wb_rd != 5'd0);
-    assign WriteReg_WB = mem_wb_rd;
-
-
-
-  
+    wire [31:0] ReadData1_ID, ReadData2_ID;
     
     wire [4:0] rs_ID = Instr_ID[25:21];
     wire [4:0] rt_ID = Instr_ID[20:16];
     wire [4:0] rd_ID = Instr_ID[15:11];
+    wire [4:0] shamt_ID = Instr_ID[10:6];
     
     RegisterFile rf(
         .Clk(Clk),
@@ -143,6 +121,60 @@ module TopLevel(
         .ReadData2(ReadData2_ID)
     );
     
+    
+    //Sign/zero extending immediate based on control signal
+    wire [31:0] Imm_ID;
+    Extension ext(
+       .in(Instr_ID[15:0]),
+       .control(ExtOp_ID),
+       .out(Imm_ID)
+    );
+    
+    //ID/EX Register
+    wire RegWrite_EX, MemWrite_EX, MemRead_EX, Branch_EX;
+    wire MemToReg_EX, ALUSrc_EX, RegDst_EX;
+    wire [3:0] ALUop_EX;
+    wire [1:0] MemSize_EX;
+    wire [31:0] ReadData1_EX, ReadData2_EX, Imm_EX, PC_EX;
+    wire [4:0] rs_EX, rt_EX, rd_EX, shamt_EX;
+    Reg_ID_EX ID_EX(
+        .clk(Clk),
+        .reset(Reset),
+        .RegWrite_in(RegWrite_ID),
+        .MemWrite_in(MemWrite_ID),
+        .MemRead_in(MemRead_ID),
+        .Branch_in(Branch_ID),
+        .MemSize_in(MemSize_ID),
+        .MemToReg_in(MemToReg_ID),
+        .ALUSrc_in(ALUSrc_ID),
+        .RegDst_in(RegDst_ID),
+        .ALUop_in(ALUop_ID),
+        .ReadData1_in(ReadData1_ID),
+        .ReadData2_in(ReadData2_ID),
+        .ExtImm_in(Imm_ID),
+        .PCp4_in(PC_ID),
+        .rs_in(rs_ID),
+        .rt_in(rt_ID),
+        .rd_in(rd_ID),
+        .shamt_in(shamt_ID),
+        .RegWrite_out(RegWrite_EX),
+        .MemWrite_out(MemWrite_EX),
+        .MemRead_out(MemRead_EX),
+        .Branch_out(Branch_EX),
+        .MemSize_out(MemSize_EX),
+        .MemToReg_out(MemToReg_EX),
+        .ALUSrc_out(ALUSrc_EX),
+        .RegDst_out(RegDst_EX),
+        .ALUop_out(ALUop_EX),
+        .ReadData1_out(ReadData1_EX),
+        .ReadData2_out(ReadData2_EX),
+        .ExtImm_out(Imm_EX),
+        .PCp4_out(PC_EX),
+        .rs_out(rs_EX),
+        .rt_out(rt_EX),
+        .rd_out(rd_EX),
+        .shamt_out(shamt_EX)
+    );
     
     
 endmodule
