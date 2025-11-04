e51cd96cf0fe3d7e8c47ce65cbc4cfd09b7d2eed
amelde <143756441+amelde@users.noreply.github.com>
2025-11-03 20:25:22 -0700

    Update ProgramCounter.v

--- ProgramCounter.v
+++ ProgramCounter.v
@@ -35,13 +35,13 @@ module ProgramCounter(Address, PCResult, Reset, Clk);
 
     // Initialize to 0 for simulation
     initial begin
-        PCResult = 32'b0;
+        PCResult = 32'h00000000;
     end
 
     // On reset, set PC to 0; otherwise, update on rising edge
     always @(posedge Clk or posedge Reset) begin
 	        if (Reset)
-	            PCResult <= 32'b0;       // start at address 0
+	            PCResult <= 32'h00000000;       // start at address 0
 	        else
 	            PCResult <= Address;     // load next PC
 	    	end
