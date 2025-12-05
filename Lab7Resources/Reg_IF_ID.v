1cd89295eba0be92cf77b942d96a43c96f1f5394
amelde <143756441+amelde@users.noreply.github.com>
2025-12-04 18:13:05 -0700

    Update Reg_IF_ID.v

--- Lab7Resources/Reg_IF_ID.v
+++ Lab7Resources/Reg_IF_ID.v
@@ -6,6 +6,7 @@ module Reg_IF_ID(
     input clk,
     input rst,
     input en,
+    input flush,
     input [31:0] instr_in,
     input [31:0] pc_in,
     input[31:0] jump_addr_in,
@@ -15,7 +16,7 @@ module Reg_IF_ID(
     );
     
     always @(posedge clk or posedge rst) begin
-        if (rst) begin //If resetting values
+        if (rst || flush) begin //If resetting values
             instr_out <= 32'b0;
             pc_out <= 32'b0;
             jump_addr_out <= 32'b0;
